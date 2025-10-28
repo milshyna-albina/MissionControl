d21fb594d12ecc6cb90ee56196fd8ee2c8269118
Mariia <minminniehee@gmail.com>
2025-10-28 14:22:50 +0400

    Save and load buttons

--- SmartTravelPlanner/Page2.xaml.cs
+++ SmartTravelPlanner/Page2.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +43,68 @@ namespace Travelling
             TotalDistanceTextBlock.Text = $"Total distance: {totalDistance} km";
  
         }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "JSON files (*.json)|*.json";
+            dialog.Title = "Save route as";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+
+                try
+                {
+                    traveler.SaveToFile(path);
+                    SaveStatusTextBlock.Text = "Route successfully saved!";
+                    SaveStatusTextBlock.Foreground = Brushes.Green;
+                    SaveStatusTextBlock.Visibility = Visibility.Visible;
+                }
+                catch (Exception)
+                {
+                    SaveStatusTextBlock.Text = "Error saving route!";
+                    SaveStatusTextBlock.Foreground = Brushes.Red;
+                    SaveStatusTextBlock.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void LoadMapButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JSON files (*.json)|*.json";
+            dialog.Title = "Select Traveler JSON file";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+
+                try
+                {
+                    Traveler traveler = Traveler.LoadFromFile(path);
+
+      
+                    LoadStatusTextBlock.Text = $"Traveler '{traveler.GetName()}' Loaded successfully!";
+
+                    string route = traveler.GetRoute();
+                    RouteTextBlock.Text = $"Current Location: {traveler.GetLocation()}\nRoute: {route}";
+                }
+                catch (FileNotFoundException ex)
+                {
+                    LoadStatusTextBlock.Text = $"Error: {ex.Message}";
+                }
+                catch (FileLoadException ex)
+                {
+                    LoadStatusTextBlock.Text = $"Error: {ex.Message}";
+                }
+                catch (Exception ex)
+                {
+                    LoadStatusTextBlock.Text = $"Unexpected error: {ex.Message}";
+                }
+            }
+        }
+
 
         private void ReturnButton_Click(object sender, RoutedEventArgs e)
         {
